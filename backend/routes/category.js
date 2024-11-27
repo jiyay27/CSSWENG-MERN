3dedaa15398b041fe0efd7f39018beee6d1c94c1
jiyay27 <116789497+jiyay27@users.noreply.github.com>
2024-11-27 20:00:40 +0800

    Category add,remove, getall

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-app.post('/api/items/add', categoryController.addCategory);
-=======
 // routes/items.js
 const express = require('express');
-const { addCategory, removeCategory } = require('../controllers/categoryController.js');
+const { addCategory, removeCategory, getCategories } = require('../controllers/categoryController.js');
 const router = express.Router();
 
 // Route to add a new item
@@ -12,5 +9,7 @@ router.post('/add', addCategory);
 // Route to delete an item
 router.delete('/delete/:id', removeCategory);
 
+// Route to delete an item
+router.get('/all', getCategories);
+
 module.exports = router;
->>>>>>> 2a7fc239eac216c86d01d2301b4d0fe9e1ea7534
